2af43f0bf9b3a9a075f2c480da4b1a3ec3a983ad
susanirago22 <susaniragoorani@gmail.com>
2022-11-24 12:46:38 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,2 +1,11 @@
 function addingEventListener() {
 }
+function addingEventListener() 
+{
+    const input =  document.getElementById("button");
+function alert_me()
+{
+    alert("I was clicked");
+}
+input.addEventListener("click",alert_me);
+}
